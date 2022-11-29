c3f76b459d2a7afd52d628479a34317f1d65fa49
Aisha Kothare <aishakothare31@gmail.com>
2022-11-29 12:39:16 -0500

    cors origin set to netlify

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ const app = express();
 
 //app.use(cors());
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://charming-marshmallow-cbf75c.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
